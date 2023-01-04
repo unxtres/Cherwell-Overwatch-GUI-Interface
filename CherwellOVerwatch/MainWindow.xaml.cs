320110795d65aa27feaeada7ee10e644b78734a6
unxtres <t.nawojczyk@gmail.com>
2023-01-04 16:15:12 +0100

    Add project files.

---
+++ CherwellOVerwatch/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CherwellOVerwatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string reglink = "http://localhost:5000/api/Registration/generate?key=" + regkey.Text;
+            using (var httpClient = new HttpClient())
+            {
+                var token = await httpClient.GetAsync(reglink);
+                result.Text= token.ToString();
+            }
+
+        }
+    }
+}
