82c6846fd4d2c41ea6f3bacc70b6e769facc19fa
unxtres <t.nawojczyk@gmail.com>
2023-04-21 12:35:18 +0200

    .

---
+++ CherwellOVerwatch/pages/OtherSettings.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using CherwellOVerwatch.Settings;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ServiceProcess;
+
+namespace CherwellOVerwatch
+{
+    public partial class OtherSettings : Page
+    {
+        public OtherSettings()
+        {
+            InitializeComponent();
+        }
+        private void Button_test(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.service_Status();
+        }
+    }
+}
