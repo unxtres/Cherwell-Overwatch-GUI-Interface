9219ef460e97a65233a752fcc5f8548f075b900a
unxtres <t.nawojczyk@gmail.com>
2023-05-18 13:31:38 +0200

    + ServiceHostLog Save

--- CherwellOVerwatch/pages/ServiceHostLog.xaml.cs
+++ CherwellOVerwatch/pages/ServiceHostLog.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Policy;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,6 +21,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 using CherwellOVerwatch.Settings;
+using Microsoft.VisualBasic.ApplicationServices;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -27,16 +30,17 @@ namespace CherwellOVerwatch
     public partial class ServiceHostLog : Page
     {
         public string url = "http://localhost:5000/api/settings/ServiceHostSettings";
+        public string json;
         public ServiceHostLog()
         {
             InitializeComponent();
         }
 
-        private void loadSettings()
+        private void Button_Load(object sender, RoutedEventArgs e)
         {
             LoadSettings loader = new LoadSettings();
-
-            Service_host DeserializedSH = JsonConvert.DeserializeObject<Service_host>(loader.GetResult(url));
+            json = loader.GetResult(url);
+            Service_host DeserializedSH = JsonConvert.DeserializeObject<Service_host>(json);
 
             eventLogLevel.Text = DeserializedSH.loggerSettings.eventLogLevel.ToString();
             fileLogLevel.Text = DeserializedSH.loggerSettings.fileLogLevel.ToString();
@@ -75,10 +79,107 @@ namespace CherwellOVerwatch
             sumoLogicLogLevel.Text = DeserializedSH.loggerSettings.sumoLogicLogLevel.ToString();
             settingsType.Text = DeserializedSH.loggerSettings.settingsType.ToString();
         }
-
-        private void Button_Load(object sender, RoutedEventArgs e)
+        private void Button_Save(object sender, RoutedEventArgs e)
         {
-            loadSettings();
+            try
+            {
+                save_status.Text = "Saving...!";
+                // Restart service
+                ServiceController service = new ServiceController("Cherwell Overwatch");
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                }
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running);
+
+                Service_host DeserializedSH = JsonConvert.DeserializeObject<Service_host>(json);
+
+                // Build JSON
+                var data = new JObject
+                {
+                    ["disableCompression"] = DeserializedSH.disableCompression,
+                    ["installed"] = DeserializedSH.installed,
+                    ["lastError"] = DeserializedSH.lastError.ToString(),
+                    ["lastErrorDetails"] = DeserializedSH.lastErrorDetails.ToString(),
+                    ["connection"] = DeserializedSH.connection.ToString(),
+                    ["encryptedPassword"] = DeserializedSH.encryptedPassword.ToString(),
+                    ["useDefaultRoleOfUser"] = DeserializedSH.useDefaultRoleOfUser,
+                    ["userId"] = DeserializedSH.userId.ToString(),
+                    ["useWindowsLogin"] = DeserializedSH.useWindowsLogin,
+                    ["hostMaxWorkers"] = DeserializedSH.hostMaxWorkers.ToString(),
+                ["loggerSettings"] = DeserializedSH.loggerSettings == null ? null : new JObject
+                    {
+                        ["eventLogLevel"] = eventLogLevel?.Text ?? "",
+                        ["fileLogLevel"] = fileLogLevel?.Text ?? "",
+                        ["fileNameOverride"] = fileNameOverride?.Text ?? "",
+                        ["isLoggingEnabled"] = isLoggingEnabled?.IsChecked,
+                        ["isServerSettings"] = isLogServerSettings?.IsChecked,
+                        ["logFilePath"] = logFilePath?.Text ?? "",
+
+                        ["logServerLogLevel"] = logServerLogLevel?.Text ?? "",
+                        ["logToComplianceLog"] = logToComplianceLog?.IsChecked,
+                        ["logToConsole"] = logToConsole?.IsChecked,
+                        ["logToConsoleLevel"] = logToConsoleLevel?.Text ?? "",
+                        ["logToEventLog"] = logToEventLog?.IsChecked,
+                        ["logToFile"] = logToFile?.IsChecked,
+                        ["logToLogServer"] = logToLogServer?.IsChecked,
+                        ["maxFilesBeforeRollover"] = maxFilesBeforeRollover?.Text ?? "",
+                        ["maxFileSizeInMB"] = maxFileSizeInMB?.Text ?? "",
+                        ["logToSumoLogic"] = logToSumoLogic?.IsChecked,
+                        ["sumoLogicLogLevel"] = sumoLogicLogLevel?.Text ?? "",
+                        ["settingsType"] = settingsType?.Text ?? "",
+                        ["logServerConnectionSettings"] = DeserializedSH.loggerSettings.logServerConnectionSettings == null ? null : new JObject
+                        {
+                            ["ignoreCertErrors"] = ignoreCertErrors?.IsChecked,
+                            ["isConfigured"] = isConfigured?.IsChecked,
+                            ["isServerSettings"] = isServerSettingsConnectionSettings?.IsChecked,
+                            ["password"] = password?.Text ?? "",
+                            ["settingsType"] = settingsType?.Text ?? "",
+                            ["url"] = urlLogServerConnectionSettings?.Text ?? "",
+                            ["userName"] = userName?.Text ?? "",
+                        },
+                        ["sumoLogicConnectionSettings"] = DeserializedSH.loggerSettings.sumoLogicConnectionSettings == null ? null : new JObject
+                        {
+                            ["url"] = urlSumoLogicConnectionSettings?.Text ?? "",
+                            ["retryInterval"] = retryInterval?.Text ?? "",
+                            ["connectionTimeout"] = connectionTimeout?.Text ?? "",
+                            ["flushingAccuracy"] = flushingAccuracy?.Text ?? "",
+                            ["maxFlushInterval"] = maxFlushInterval?.Text ?? "",
+                            ["messagesPerRequest"] = messagesPerRequest?.Text ?? "",
+                            ["maxQueueSizeBytes"] = maxQueueSizeBytes?.Text ?? "",
+                        }
+                    }
+                };
+
+                var settingData = new JObject
+                {
+                    ["setting"] = JsonConvert.SerializeObject(data),
+                    ["publish"] = true
+                };
+
+                var jsonData = JsonConvert.SerializeObject(settingData);
+
+                // Send request
+                var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpRequest.Method = "POST";
+                httpRequest.Accept = "application/json";
+                httpRequest.Headers["Authorization"] = TokenInterface.OWToken;
+                httpRequest.ContentType = "application/json";
+
+                using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(jsonData);
+                }
+
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                save_status.Text = httpResponse.StatusCode.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Not Connected");
+            }
         }
     }
 }
